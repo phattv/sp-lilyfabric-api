4237930c5c8599ca6dba564b4f0f76225025b290
Phat Tran <PHAT@Phat.local>
2017-06-01 23:43:51 +0700

    add POST PUT DELETE /dresses

--- routes/dresses.js
+++ routes/dresses.js
@@ -17,6 +17,45 @@ router
       .then(function (dresses) {
         response.json(dresses);
       })
+  })
+  .post(function (request, response) {
+    new Dress({
+      code: request.body.code,
+      name: request.body.name,
+      description: request.body.description
+    })
+      .save()
+      .then(function (savedDress) {
+        console.log(savedDress);
+        response.json(savedDress);
+      });
+  });
+
+router
+  .route('/:id')
+  .put(function (request, response) {
+    Dress
+      .where('id', request.params.id)
+      .fetch()
+      .then(function (dress) {
+        dress
+          .save({
+            code: request.body.code,
+            name: request.body.name,
+            description: request.body.description
+          })
+          .then(function (savedDress) {
+            response.json(savedDress);
+          })
+      })
+  })
+  .delete(function (request, response) {
+    Dress
+      .where('id', request.params.id)
+      .destroy()
+      .then(function (destroyedDress) {
+        response.json(destroyedDress);
+      })
   });
 
 module.exports = router;

7f002b28428a505a0848afbb3030e4f2fb00ce12
Phat Tran <PHAT@Phat.local>
2017-06-28 01:29:14 +0700

    leaving bookshelfjs

--- bookshelf.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-//<editor-fold desc="node_modules">
-var knex = require('knex')(require('./knexfile')),
-  bookshelf = require('bookshelf')(knex);
-//</editor-fold>
-
-module.exports = bookshelf;
--- knexfile.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: 'database.sqlite3'
-  },
-  useNullAsDefault: true
-};
